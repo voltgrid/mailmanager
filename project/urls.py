6926aa06602e8d028d4a1a3570d41b8b26e8e855
Andrew Cutler <macropin@gmail.com>
2015-09-17 20:57:00 +1000

    Remove sample template. Add password reset. Redirect to login. Change admin title.

--- project/urls.py
+++ project/urls.py
@@ -1,18 +1,17 @@
 from django.conf.urls import patterns, include, url
-from django.views.generic import TemplateView
+from django.core.urlresolvers import reverse_lazy
+from django.views.generic import RedirectView
 
 from django.contrib import admin
 admin.autodiscover()
 
+admin.site.site_title = 'Mail Manager'
+admin.site.site_header = 'Mail Manager'
+admin.site.index_title = 'Administration'
+
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'project.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^$',
-        TemplateView.as_view(template_name='index.html'),
-        name='index'),
-
-
+    url(r'^$', RedirectView.as_view(url=reverse_lazy('admin:login'), permanent=False)),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/password_reset/$', 'django.contrib.auth.views.password_reset', name='admin_password_reset'),
+    url(r'^accounts/', include('django.contrib.auth.urls')),
 )
